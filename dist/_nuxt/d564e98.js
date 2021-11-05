9e4ce98b1d115e99d896c6f9b9d49687feb452bd
Worlanyo <worladamalie@gmail.com>
2021-11-05 17:27:34 +0000

    generate nuxt dist

---
+++ dist/_nuxt/d564e98.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{432:function(e,t,n){"use strict";n.r(t);var l={middleware:"auth"},r=n(20),component=Object(r.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("PropertyDetail")}),[],!1,null,null,null);t.default=component.exports;installComponents(component,{PropertyDetail:n(396).default})}}]);
